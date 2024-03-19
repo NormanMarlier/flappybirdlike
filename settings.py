26b1f3f482efa03535c09e52289ad7043c13b525
Norman <norman.marlier@gmail.com>
2024-03-19 17:25:31 +0100

    Settings of the game

---
+++ settings.py
@@ -0,0 +1,15 @@
+# Screen
+GAME_W: int = 500
+GAME_H: int = 500
+SCREEN_W: int = 960
+SCREEN_H: int = 720
+
+# Framerate
+FRAMERATE: int = 60
+
+# Motion
+GRAVITY: float = 600.
+JUMPING_HEIGHT: float = 400.
+
+# Save
+SAVE_FILE: str = "save.json"
